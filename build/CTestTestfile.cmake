401eddefb347aacc6abfc1b1ca8942d163b8e812
1113870631 <1113870631@qq.com>
2022-04-21 12:59:41 +0800

    再分份

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ww/smart/camera_all
-# Build directory: /home/ww/smart/camera_all/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
