85563463b0f64947f9733b9a7257ddc01b01e0e6
1113870631 <1113870631@qq.com>
2022-05-04 09:58:51 +0800

    test

--- src/Obstacle_detection.cpp
+++ src/Obstacle_detection.cpp
@@ -62,37 +62,48 @@ using namespace cv;
  * @param color_picture   显示的图片
  */
     void Obstacle_dis_rectangle(vector<cv::Vec4f> Obstacles,Mat color_picture,Mat disp){
+        imshow("row",disp);
         vector<Vec4f>::iterator it0;
         for(it0=Obstacles.begin();it0!=Obstacles.end();it0++){
             rectangle(color_picture,Point((*it0)[0],(*it0)[1]),Point((*it0)[2],(*it0)[3]),Scalar(0,255,0),3,8,0);
             //得到障碍物区域 
             Mat rol_obs=disp.rowRange((*it0)[3],(*it0)[1]);
-           rol_obs=disp.colRange((*it0)[2],(*it0)[0]);
+           rol_obs=rol_obs.colRange((*it0)[2],(*it0)[0]);
+           rol_obs.convertTo(rol_obs,CV_8UC1);
             //进行视差值处理       求x交叉线所有元素的均值  小于均值的元素设为0  剩下的元素求均值
-            float tmp1=0,num=0;
-                //y=kx y=kx+rows             
-            for(int x=0;x<rol_obs.cols;x++)
-                for(int y=0;y<rol_obs.rows;y++){
-                    if(y==rol_obs.rows/rol_obs.cols*x||y==-rol_obs.rows/rol_obs.cols*x+rol_obs.rows){
-                        tmp1+=rol_obs.at<short>(x,y);
-                        num++;
-                    }
+           //中值滤波
+            medianBlur(rol_obs,rol_obs,5);
+
+/*             rol_obs.convertTo(rol_obs,CV_8UC1);
+             Mat im_color;
+            applyColorMap(rol_obs, im_color, COLORMAP_JET);
+             imshow("re",im_color); */
+                int height=rol_obs.rows;
+                int length=rol_obs.cols;
+                int tmp1=0,tmp2=0;
+                for(int i=length/4;i<(int)(length*3/4);i++){
+                    cout<<rol_obs.at<short>((int)(height/2),i)<<endl;
                 }
-                tmp1/=num;
-         double tmp2=0;
+               // cout<<tmp1<<endl;
+                  for(int i=height/4;i<(int)(height*3/4);i++){
+                    tmp2+=rol_obs.at<short>(i,(int)length/2);
+                }
+                tmp1=2*(tmp1+tmp2)/(height+length)/16;
+
+       /*   double tmp2=0;
                 num=0;
                 threshold(rol_obs,rol_obs,tmp1,tmp1,THRESH_TOZERO_INV);
                 for(int x=0;x<rol_obs.cols;x++)
                     for(int y=0;y<rol_obs.rows;y++){
-                        if(rol_obs.at<short>(x,y)!=0)
+                        if(rol_obs.at<short>(x,y)!=0&&rol_obs.at<short>(x,y)>0)
                         tmp2+=rol_obs.at<short>(x,y);
                         num++;
                     }
-                    tmp2/=num*16;
+                    tmp2/=num*16; */
                     //cout<<tmp2<<endl;
-                    double x=tmp2;
-                    double depth=(5e-10)*x*x*x*x*x*x- 3e-07*x*x*x*x*x + 7e-05*x*x*x*x - 0.0087*x*x*x + 0.63*x*x - 26.27*x + 582.63; 
-                string distance = to_string((int)tmp2);
+                 /*    double x=tmp2;
+                    double depth=(5e-10)*x*x*x*x*x*x- 3e-07*x*x*x*x*x + 7e-05*x*x*x*x - 0.0087*x*x*x + 0.63*x*x - 26.27*x + 582.63;  */
+                string distance = to_string((int)tmp1);
                 putText(color_picture, distance, Point((*it0)[0]+20,(*it0)[1] ),  FONT_HERSHEY_SIMPLEX, 1.0f, Scalar (255,255,0), 3, 8,false);
 
            
